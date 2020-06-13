a7409f7c9df248061b4ef30339e186b703018ab1
Chelsey Grice <chelseygrice92@gmail.com>
2020-06-13 12:58:11 +0200

    backen commit

---
+++ data.js
@@ -0,0 +1,31 @@
+export default {
+    products: [ {
+        _id:'1',
+        name:'Art 1',
+        image:'https://static.boredpanda.com/blog/wp-content/uploads/2019/12/47693137_1838245459618198_5701545794225180157_n-5e0b20ec13c79__700.jpg',
+        category:'Landscape',
+        artist: 'Chelsey',
+        price: 100000000.00,
+        countInStock: 6,
+    },
+    {
+        _id:'2',
+        name:'Art 1',
+        image:'https://static.boredpanda.com/blog/wp-content/uploads/2019/12/47693137_1838245459618198_5701545794225180157_n-5e0b20ec13c79__700.jpg',
+        category:'Landscape',
+        artist: 'Chelsey',
+        price: 100000000.00,
+        countInStock: 0,
+    },
+    {
+        _id:'3',
+        name:'Art 1',
+        image:'https://static.boredpanda.com/blog/wp-content/uploads/2019/12/47693137_1838245459618198_5701545794225180157_n-5e0b20ec13c79__700.jpg',
+        category:'Landscape',
+        artist: 'Chelsey',
+        price: 100000000.00,
+        countInStock: 6,
+    }
+]
+
+};
